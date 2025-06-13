b1b18f0395028f75944becfad5f875aad90e2f2a
Julieth Campos Pérez <102711785+JuliethCP@users.noreply.github.com>
2025-06-13 17:28:54 -0600

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,124 @@
 # Prueba-Tecnica-Ecommerce
+
+# Ecommerce Platform
+
+## Descripción del Proyecto
+Este es un proyecto de comercio electrónico de productos relacionados con el mundo del K POP, este sistema permite a los usuarios explorar productos, añadirlos a un carrito de compras y comprarlos. El sistema incluye un frontend desarrollado con Next.js y un backend construido con Node.js, Express y Prisma para la gestión de la base de datos.
+
+## Tecnologías Utilizadas
+### Frontend
+- **Next.js**: Framework de React para aplicaciones web.
+- **JavaScript**: Lenguaje principal para el desarrollo del frontend.
+- **Bootstrap**: Para el diseño y estilos.
+
+### Backend
+- **Node.js**: Entorno de ejecución para JavaScript.
+- **Express.js**: Framework para construir APIs REST.
+- **Prisma**: ORM para la gestión de la base de datos.
+- **PostgreSQL**: Base de datos relacional.
+
+## Instrucciones para Ejecutar el Proyecto
+
+### Configuración Inicial
+1. Clona este repositorio:
+   ```bash
+   git clone <URL_DEL_REPOSITORIO>
+   cd <NOMBRE_DEL_REPOSITORIO>
+   ```
+
+2. Asegúrate de tener instalados:
+   - Node.js (v16 o superior)
+   - PostgreSQL
+
+### Backend
+1. Ve al directorio del backend:
+   ```bash
+   cd backend
+   ```
+
+2. Instala las dependencias:
+   ```bash
+   npm install
+   ```
+
+3. Configura las variables de entorno:
+   - Crea un archivo `.env` en el directorio `backend` con el siguiente contenido:
+     ```
+     DATABASE_URL=postgresql://<USUARIO>:<CONTRASEÑA>@<HOST>:<PUERTO>/<NOMBRE_BASE_DE_DATOS>
+     ```
+
+4. Ejecuta las migraciones de la base de datos (esto creará las tablas necesarias para trabajar):
+   ```bash
+   npx prisma migrate dev
+   ```
+
+5. Abrir su administrador de base de datos ya sea PdAdmin u otros y ejecuta el archivo insert products.sql ubicado en la raiz del proyecto.
+
+6. Inicia el servidor:
+   ```bash
+   npm run dev
+   ```
+   El backend estará disponible en `http://localhost:4000`.
+
+
+### Frontend
+1. Ve al directorio del frontend:
+   ```bash
+   cd frontend
+   ```
+
+2. Instala las dependencias:
+   ```bash
+   npm install
+   ```
+
+3. Inicia el servidor de desarrollo:
+   ```bash
+   npm run dev
+   ```
+   El frontend estará disponible en `http://localhost:3000`.
+
+## Detalles de Configuración de Base de Datos
+El proyecto utiliza PostgreSQL como base de datos. Prisma se encarga de gestionar las migraciones y el esquema de la base de datos.
+
+### Scripts de Configuración
+1. **Generar el cliente de Prisma**:
+   ```bash
+   npx prisma generate
+   ```
+
+2. **Ejecutar migraciones**:
+   ```bash
+   npx prisma migrate dev
+   ```
+
+3. **Explorar la base de datos con Prisma Studio**:
+   ```bash
+   npx prisma studio
+   ```
+
+### Esquema de la Base de Datos
+El esquema de la base de datos se encuentra definido en el archivo [`schema.prisma`](backend/prisma/schema.prisma). Incluye los modelos `User`, `Product` y `Cart` para gestionar usuarios, productos y carritos de compra.
+
+
+### Crear un Admin
+Para crear un usuario administrador en el sistema, sigue estos pasos:
+
+1. Asegúrate de que el backend esté configurado correctamente y que las migraciones de la base de datos hayan sido ejecutadas.
+
+2. Ejecuta el script `create-admin.js` ubicado en el directorio `backend/scripts`. Este script creará un usuario administrador con credenciales predeterminadas.
+
+   ```bash
+   node backend/scripts/create-admin.js
+   ```
+
+3. Por defecto, el usuario administrador tendrá las siguientes credenciales:
+   - **Email**: `admin@kpopbeat.com`
+   - **Contraseña**: `admin123`
+
+4. Puedes iniciar sesión con estas credenciales para acceder a las funciones de administrador.
+
+> **Nota**: Si deseas personalizar las credenciales del administrador, edita el archivo `create-admin.js` antes de ejecutarlo.
+
+
+¡Listo! Ahora puedes ejecutar y explorar el proyecto.
---
+++ insert products.sql
@@ -0,0 +1,87 @@
+select * from public."Product"
+
+INSERT INTO public."Product"(name, description, price, category, "imageUrl", stock) VALUES
+('BLACKPINK ZIP-UP HOODIE', 'COTTON - M: Chest 63 cm, Length 70 cm, Shoulder 58 cm, Sleeve 54 cm 
+ L: Chest 66 cm, Length 72 cm, Shoulder 61 cm, Sleeve 56.5 cm', 95, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789742/bp-hoodie_fw07nl.png', 428),
+('BLACKPINK BUCKET HAT', 'COTTON - ONE Details2: 58 cm', 42, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789742/bp-hat_zzzxf0.png', 349),
+('BLACKPINK T-SHIRT', 'COTTON - M: Chest 51 cm, Length 71.5 cm, Shoulder 46 cm, Sleeve 23 cm 
+ L: Chest 53 cm, Length 73.5 cm, Shoulder 47 cm, Sleeve 24 cm', 39, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789752/bp-t-shirt_d1quna.png', 123),
+('BLACKPINK BEANIE', 'ACRYLIC 100% - FREE', 23, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789742/bp-beanie_ph5ux9.png', 132),
+('BLACKPINK CHARACTER DOLL', 'POLYESTER 100% - SOOYA : 130 x 220 x 110 mm 
+ GOMDEUKI : 135 x 220 x 110 mm 
+ ROSIE : 140 x 220 x 180 mm 
+ PPEU : 130 x 220 x 120 mm', 36, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789742/bp-dolls_nlypm1.png', 84),
+('BLACKPINK CHARACTER KEYRING', 'POLYESTER 100% - SOOYA : 70 x 110 x 55 mm 
+ GOMDEUKI : 70 x 110 x 55 mm 
+ ROSIE : 70 x 110 x 95 mm 
+ PPEU : 70 x 110 x 70 mm', 14, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789742/bp-keyring_aa3dun.png', 212),
+('BLACKPINK STICKER PACK', 'PAPER, PP, PVC - MINIMUM 73*50 
+ MAXIMUM 131*75', 8, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789752/bp-stickers_c1kdpj.png', 414),
+('BLACKPINK TOUR HOODIE', 'COTTON 100% - M: Chest 64 cm, Length 69.5 cm, Shoulder 18 cm, Sleeve 54 cm 
+ L: Chest: Chest 66 cm, Length 71.5 cm, Shoulder 20 cm, Sleeve 56 cm', 73, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789742/bp-hoodie2_o8dlrl.png', 215),
+('BLACKPINK REUSABLE BAG', 'R-PET - 500 x 150x 400 mm', 8, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749791036/bp-bag_ok9hvz.png', 349),
+('BLACKPINK PHOTO BINDER', 'PVC, METAL - BINDER: 140 * 140 mm 
+ PHOTO CARD: 55 * 85 mm', 23, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789742/bp-binder_ycx2ug.png', 316),
+('BLACKPINK PHOTO PACKAGE', 'PAPER - A3 POSTER: 297 * 420 mm 
+ POST CARD: 100 * 150 mm 
+ POLAROID PHOTO: 90 * 106 mm 
+ 2 CUT PHOTO: 50 * 150 mm 
+ PHOTO CARD: 55 * 85 mm', 23, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789743/bp-photos_sbigvs.png', 257),
+('BLACKPINK SCRUNCHIE', 'POLYESTER - SCRUNCHIE: 160 * 160 mm 
+ DAENG-GI: 45 * 880 mm', 19, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789743/bp-scrunchie_fekzl9.png', 169),
+('BLACKPINK ALBUM BORN PINK', 'BORN PINK - YG Entertainment', 49, 'ALBUMS', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789741/bp-album_sv5ijt.png', 843),
+('(G)I-DLE MINI ALBUM I AM', 'LATATA - CUBE Ent.', 13, 'ALBUMS', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789759/g-idle-IAm_f7uyer.png', 842),
+('(G)I-DLE MINI ALBUM I TRUST', 'OH MY GOD - CUBE Ent.', 14, 'ALBUMS', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789767/g-idle-ITrust_smky2l.png', 840),
+('(G)I-DLE SINGLE ALBUM DUMDI DUMDI', 'DUMDi DUMDi - CUBE Ent.', 14, 'ALBUMS', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789759/g-idle-dumdi_zds8aa.png', 642),
+('(G)I-DLE MINI ALBUM I LOVE', 'NXDE - CUBE Ent.', 17, 'ALBUMS', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789767/g-idle-ILove_uco905.png', 348),
+('(G)I-DLE ALBUM I FEEL BUTTERFLY VER.', 'TO BE ANNOUNCED - CUBE Ent.', 17, 'ALBUMS', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789767/g-idle-IFeel_zm5qj4.png', 532),
+('(G)I-DLE SPECIAL ALBUM HEAT', 'TO BE ANNOUNCED - CUBE Ent.', 18, 'ALBUMS', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789759/g-idle-Heat_ks4lcx.png', 621),
+('(G)I-DLE MINI ALBUM I BURN', 'HWA - CUBE Ent.', 14, 'ALBUMS', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789759/g-idle-Burn_oc6rou.png', 613),
+('(G)I-DLE ALBUM I FEEL CAT VER.', 'TO BE ANNOUNCED - CUBE Ent.', 17, 'ALBUMS', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789767/g-idle-IFeel-Cat_xjt9vo.png', 526),
+('(G)I-DLE ALBUM I FEEL QUEEN VER.', 'TO BE ANNOUNCED - CUBE Ent.', 17, 'ALBUMS', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789767/g-idle-IFeel-Queen_uo1imu.png', 416),
+('(G)I-DLE ALBUM I FEEL SET', 'TO BE ANNOUNCED - CUBE Ent.', 35, 'ALBUMS', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789767/g-idle-IFeelSet_tkzuqq.png', 735),
+('LE SSERAFIM OFFICIAL LIGHT STICK', 'ABS, PC - About 4.96 x 25.7 x 4.2 cm', 46, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789777/LS-LightStick_zuenmi.png', 258),
+('LE SSERAFIM KEYRING', 'ZINC ALLOY, STEEL - 3 x 10.9 cm', 19, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789776/LS-keyring_kxzthg.png', 632),
+('LE SSERAFIM BADGE SET', 'ZINC ALLOY, STEEL - Tour ver. : 6.5 x 1.1 cm 
+ Symbol ver. : 6.5 x 1.1, 10 x 9 x 1.4 cm', 20, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789767/LS-Badge_d0uaos.png', 312),
+('LE SSERAFIM BOTTLE', 'STAINLESS STEEL, SILICONE - 7 x 21.7, 500ml', 33, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789776/LS-Bottle_aqajog.png', 132),
+('LE SSERAFIM SHOPPER BAG', '90% POLYESTER / 10% NYLON - Width: 51 cm, Depth: 17 cm, Height: 39.5 cm, Strap length: 63 cm', 13, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789785/LS-ShopperBag_ks6obb.png', 578),
+('LE SSERAFIM BUCKET HAT', 'Shell: 100% Polyester 
+ Lining: 100% Cotton - Inner Circumference: 58 cm, Brim Width: 6.5 cm, Height: 9 cm', 42, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789777/LS-BucketHat_a3miqe.png', 114),
+('LE SSERAFIM LANDYARD HOLDER', 'PVC, PE, POM, ZINC ALLOY, BRASS, PAPER - HOLDER: 7 x 11.3 cm 
+ LANYARD: 2 x 50 cm 
+ PHOTOCARD: 5.4 x 8.6 cm', 20, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789777/LS-Landyard_ktsgif.png', 634),
+('LE SSERAFIM PHOTO CARD BINDER', 'PAPER, PP, PVC, STEEL, ACRYLIC, POLYESTER - 18.1 x 24.7 x 4 cm', 30, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789768/LS-Binder_katipq.png', 342),
+('LE SSERAFIM MINI PHOTO CARD SET', 'PAPER - 5.4 x 8.6 cm', 74, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789777/LS-PhotoCard_m5smps.png', 542),
+('LE SSERAFIM STICKER PACK', 'PAPER - MIN: 2.1 x 2.7 cm 
+ MAX: 3.9 x 9.2 cm', 14, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789785/LS-Stickers_cq5umw.png', 835),
+('LE SSERAFIM POSTCARD BOOK', 'PAPER - 15.5 x 21.5 x 0.6', 20, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789777/LS-Photobook_zgoicc.png', 412),
+('NEW JEANS HOODIE', 'COTTON 100% - M: Length 68 cm, Shouler width 79 cm, Chest width 70 cm, Bottom width 54 cm, Sleeve length 47 cm 
+ L: Length 73 cm, Shouler width 84 cm, Chest width 77 cm,  Bottom width 57 cm, Sleeve length 54 cm 
+ STICKER : 9 x 6.9', 115, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789785/NJ-Hoodie_eoz7ou.png', 314),
+('NEW JEANS TOKKI PHONE CASE', 'TPU - IPHONE 13 : 7.5*15*1 
+ IPHONE 13 PRO : 7.5*15*1 
+ IPHONE 13 PRO MAX : 8*16.3*1 
+ IPHONE 13 MINI : 6.3*13.3*1 
+ IPHONE 14 : 7.5*15*1 
+ IPHONE 14 PRO : 7.5*15*1 
+ IPHONE 14 PRO MAX : 8*16.3*1 
+ IPHONE 14 PLUS : 8*16.3*1', 25, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789785/NJ-Case_lcc6z6.png', 342),
+('NEW JEANS BRACELET', 'SURGICAL STEEL - Ball chain bracelet length: 20 cm 
+ Initial charm: 1.5 x 0.5 x 0.15 cm', 27, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789785/NJ-Bracelet_hnmsms.png', 623),
+('NEW JEANS SMARTTOK (TOKKI)', 'ACRYLIC, PC, TPU - 7.3 * 6.5 cm', 18, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789810/NJ-smarttok_uvvhim.png', 743),
+('NEW JEANS FLUFFY KEYRING', '100% Polyester - 15 * 11.85 cm', 22, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789785/NJ-keyring_bqaeto.png', 424),
+('NEW JEANS TOKKI KEY', 'ZINC ALLOY, STAINLESS STEEL - KEY : 2.8 x 5 x 0.4 cm 
+ DICE : 1 x 1 x 1 cm', 20, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789826/NJ-TOKKIKEY_clnyke.png', 622),
+('NEW JEANS NECKLACE', 'SURGICAL STEEL - LOCKET: Length: 2.5 cm, Height: 2.9 cm, Width: 1.2 cm 
+ CHAIN LENGTH: 80 cm', 33, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789792/NJ-Necklace_g18u3l.png', 523),
+('NEW JEANS SITTING CUSHION', 'POLYESTER, POLYURETHANE - 40 * 40 cm', 22, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789792/NJ-sitting_xe5zgj.png', 412),
+('NEW JEANS ACRYLIC RING', 'ACRYLIC - Inner Diameter: 1.6, Thickness: 0.3', 10, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789792/NJ-Ring_iln2sd.png', 643),
+('NEW JEANS ACRYLIC TOPPER SET', 'ACRYLIC - 2 * 6 cm', 28, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789826/NJ-TOPPER_zjrfvi.png', 512),
+('NEW JEANS OMG POSTER', 'PAPER - 59.4 x 42 cm', 18, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789792/NJ-Poster_flnw6p.png', 842),
+('NEW JEANS LIGHTSTICK', 'ABS, PC - 11 x 23.1 x 5.27 cm', 45, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789791/NJ-lightstick_zwl4zv.png', 643),
+('NEW JEANS SWEATSHIRT', 'COTTON 100% - M: Chest 63 cm, Length 70 cm, Shoulder 58 cm, Sleeve 54 cm 
+ L: Chest 66 cm, Length 72 cm, Shoulder 61 cm, Sleeve 56.5 cm', 64, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789826/NJ-SweatShirt_pelikl.png', 745),
+('NEW JEANS DICE KEYRING', 'ZINC ALLOY, STAINLESS STEEL - NJ TOKKI CHARM: 3 x 3 x 1.1 cm 
+ DICE CHARM: 1.5 x 1.5 x1.5 cm 
+ CHAIN: 1 x 4.5 x 0.8 cm', 21, 'MERCH', 'https://res.cloudinary.com/dyrhcukee/image/upload/v1749789785/NJ-keyringDice_iembex.png', 251);
